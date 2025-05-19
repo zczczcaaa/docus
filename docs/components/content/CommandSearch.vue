e16717df0ae90cea71bf9c83eb8056c4a3d59202
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-05-19 16:10:47 +0200

    fix(landing): neutral and primary in iframe command menu

--- docs/components/content/CommandSearch.vue
+++ docs/components/content/CommandSearch.vue
@@ -1,8 +1,16 @@
 <template>
   <ClientOnly>
     <iframe
-      src="https://ui.nuxt.com/examples/content-search-example"
+      :src="src"
       class="rounded-lg relative h-96 w-full"
     />
   </ClientOnly>
 </template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+
+const src = computed(() => {
+  return `https://ui.nuxt.com/examples/content-search-example?primary=${appConfig.ui.colors.primary}&neutral=${appConfig.ui.colors.neutral}`
+})
+</script>
