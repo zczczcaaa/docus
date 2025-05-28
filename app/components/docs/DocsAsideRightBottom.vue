28f521dd51aa4ce5b7354416bd3bf626f9f79cd0
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-05-28 15:59:41 +0200

    feat(docs): links from app config from docs aside right bottom

--- app/components/docs/DocsAsideRightBottom.vue
+++ app/components/docs/DocsAsideRightBottom.vue
@@ -1,3 +1,17 @@
 <template>
-  <div />
+  <div
+    v-if="appConfig.toc?.bottom?.links?.length"
+    class="hidden lg:block space-y-6"
+  >
+    <USeparator type="dashed" />
+
+    <UPageLinks
+      :title="appConfig.toc?.bottom?.title || 'Links'"
+      :links="appConfig.toc?.bottom?.links"
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
