d40715593adecf5e8421e100e897687a28a56e39
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-06-17 20:24:08 +0200

    fix(setup): docs layer

--- cli/setup.ts
+++ cli/setup.ts
@@ -12,6 +12,19 @@ const appDir = fileURLToPath(new URL('../app', import.meta.url))
 const pkgDir = fileURLToPath(new URL('..', import.meta.url))
 
 export async function getNuxtConfig(dir: string, _opts: DocsOptions = {}) {
+  const fixLayers = (_, nuxt) => {
+    const hasDocsDir = nuxt.options._layers.some(layer => layer.cwd === dir)
+    if (!hasDocsDir) {
+      nuxt.options._layers.unshift({
+        cwd: dir,
+        config: {
+          rootDir: dir,
+          srcDir: dir,
+        },
+      })
+    }
+  }
+
   // @ts-expect-error __DOCS_DIR__ is not defined
   global.__DOCS_DIR__ = resolve(dir, 'content')
 
@@ -20,5 +33,6 @@ export async function getNuxtConfig(dir: string, _opts: DocsOptions = {}) {
     compatibilityDate: '2025-06-17',
     extends: [appDir],
     modulesDir: [resolve(pkgDir, 'node_modules'), resolve(appDir, 'node_modules')],
+    modules: [fixLayers],
   } as NuxtConfig
 }
