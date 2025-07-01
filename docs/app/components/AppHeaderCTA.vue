c2aa7df76822ee6391587737e8f23335f1242fe7
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-07-01 09:57:09 +0200

    docs(app): redirect to installation instead of introduction

--- docs/app/components/AppHeaderCTA.vue
+++ docs/app/components/AppHeaderCTA.vue
@@ -1,7 +1,7 @@
 <template>
   <UButton
     v-if="$route.path == '/'"
-    to="/getting-started/introduction"
+    to="/getting-started/installation"
     color="neutral"
     size="sm"
     class="hidden lg:inline-flex"
