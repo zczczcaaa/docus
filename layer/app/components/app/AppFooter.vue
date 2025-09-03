7815063edf4a496ed250dfa114752333af4b3ce3
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-09-03 16:15:54 +0200

    feat(customization): `AppFooterLeft` and `AppFooterRight` components

--- layer/app/components/app/AppFooter.vue
+++ layer/app/components/app/AppFooter.vue
@@ -1,40 +1,11 @@
-<script setup lang="ts">
-const appConfig = useAppConfig()
-
-const links = computed(() => [
-  ...Object.entries(appConfig.socials || {}).map(([key, url]) => ({
-    'icon': `i-simple-icons-${key}`,
-    'to': url,
-    'target': '_blank',
-    'aria-label': `${key} social link`,
-  })),
-  appConfig.github?.url && {
-    'icon': 'i-simple-icons-github',
-    'to': appConfig.github.url,
-    'target': '_blank',
-    'aria-label': 'GitHub repository',
-  },
-].filter(Boolean))
-</script>
-
 <template>
   <UFooter>
     <template #left>
-      <div class="text-sm text-muted">
-        Copyright © {{ new Date().getFullYear() }}
-      </div>
+      <AppFooterLeft />
     </template>
 
     <template #right>
-      <template v-if="links.length">
-        <UButton
-          v-for="(link, index) of links"
-          :key="index"
-          size="sm"
-          v-bind="{ color: 'neutral', variant: 'ghost', ...link }"
-        />
-      </template>
-      <UColorModeButton />
+      <AppFooterRight />
     </template>
   </UFooter>
 </template>
