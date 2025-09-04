d3dd4ed17484c3f25b4c39df812d3177ceb7a974
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-09-04 18:24:11 +0200

    feat(imports): only add useDocusI18n if not already imported

--- layer/modules/routing.ts
+++ layer/modules/routing.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, extendPages, createResolver, addImports } from '@nuxt/kit'
+import { defineNuxtModule, extendPages, createResolver } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
@@ -9,9 +9,14 @@ export default defineNuxtModule({
 
     const isI18nEnabled = !!(nuxt.options.i18n && nuxt.options.i18n.locales)
 
-    addImports({
-      name: 'useDocusI18n',
-      from: resolve('../app/composables/useDocusI18n'),
+    // Ensure useDocusI18n is available in the app
+    nuxt.hook('imports:extend', (imports) => {
+      if (imports.some(i => i.name === 'useDocusI18n')) return
+
+      imports.push({
+        name: 'useDocusI18n',
+        from: resolve('../app/composables/useDocusI18n'),
+      })
     })
 
     extendPages((pages) => {
