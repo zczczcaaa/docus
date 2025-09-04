7081457dc6e2278bba15a984f95220355813b806
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-09-04 12:39:58 +0200

    feat(app): handle files search and translations on error page

--- layer/app/error.vue
+++ layer/app/error.vue
@@ -1,42 +1,66 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app'
+import type { PageCollections } from '@nuxt/content'
+import * as locales from '@nuxt/ui-pro/locale'
 
-defineProps<{
+const props = defineProps<{
   error: NuxtError
 }>()
 
+const localizedError = computed(() => {
+  return {
+    ...props.error,
+    statusMessage: t('common.error.title'),
+  }
+})
+
+const lang = computed(() => locales[locale.value as keyof typeof locales]?.code || 'en')
+const dir = computed(() => locales[locale.value as keyof typeof locales]?.dir || 'ltr')
+
 useHead({
   htmlAttrs: {
-    lang: 'en',
+    lang,
+    dir,
   },
 })
 
 useSeoMeta({
-  title: 'Page not found',
-  description: 'We are sorry but this page could not be found.',
+  title: () => t('common.error.title'),
+  description: () => t('common.error.description'),
 })
 
-// const { data: navigation } = await useAsyncData('navigation', () => queryCollectionNavigation('docs'))
-// const { data: files } = useLazyAsyncData('search', () => queryCollectionSearchSections('docs'), {
-//   server: false,
-// })
+const { locale, isEnabled, t } = useDocusI18n()
 
-// provide('navigation', navigation)
+const collectionName = computed(() => isEnabled.value ? `docs_${locale.value}` : 'docs')
+
+const { data: navigation } = await useAsyncData(`navigation_${collectionName.value}`, () => queryCollectionNavigation(collectionName.value as keyof PageCollections), {
+  transform: (data) => {
+    const rootResult = data.find(item => item.path === '/docs')?.children || data || []
+
+    return rootResult.find(item => item.path === `/${locale.value}`)?.children || rootResult
+  },
+  watch: [locale],
+})
+const { data: files } = useLazyAsyncData(`search_${collectionName.value}`, () => queryCollectionSearchSections(collectionName.value as keyof PageCollections), {
+  server: false,
+})
+
+provide('navigation', navigation)
 </script>
 
 <template>
   <UApp>
     <AppHeader />
 
-    <UError :error="error" />
+    <UError :error="localizedError" />
 
     <AppFooter />
 
     <ClientOnly>
-      <!-- <LazyUContentSearch
+      <LazyUContentSearch
         :files="files"
         :navigation="navigation"
-      /> -->
+      />
     </ClientOnly>
   </UApp>
 </template>
